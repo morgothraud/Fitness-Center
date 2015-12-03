4cabd620207400a16363fc90ce6db190d50539b4
morgothraud <biryolkaan@gmail.com>
2015-12-04 01:07:21 +0200

    forgotpassword

---
+++ FitnessCenterv2/FitnessCenterv2/Models/ChangePasswordModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FitnessCenterv2.Models
+{
+    public class ChangePasswordModel
+    {
+        public User user { get; set; }
+        public PassReset passReset { get; set; }
+    }
+}
--- FitnessCenterv2/FitnessCenterv2/PassReset.cs
+++ FitnessCenterv2/FitnessCenterv2/PassReset.cs
@@ -12,12 +12,15 @@ namespace FitnessCenterv2
     using System;
     using System.Collections.Generic;
     
-    public partial class sysdiagram
+    public partial class PassReset
     {
-        public string name { get; set; }
-        public int principal_id { get; set; }
-        public int diagram_id { get; set; }
-        public Nullable<int> version { get; set; }
-        public byte[] definition { get; set; }
+        public int ID { get; set; }
+        public Nullable<int> UserID { get; set; }
+        public string EMail { get; set; }
+        public string AutID { get; set; }
+        public Nullable<bool> isAvaliable { get; set; }
+    
+        public virtual User User { get; set; }
+        public virtual User User1 { get; set; }
     }
 }
